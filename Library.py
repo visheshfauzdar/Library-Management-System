8f37f6aba2fe8493c728c51d47cd5b9217f1778d
visheshfauzdar <you@example.com>
2022-10-15 11:45:31 +0530

    ui of members page

--- Library.py
+++ Library.py
@@ -2,56 +2,100 @@ from pydoc import text
 from re import search
 from tkinter import *
 from turtle import title
+
 class Multiple():
     def __init__(self,root):
         self.root=root
         self.root.geometry("900x700")
         self.root.title("Library Management System")
         self.root.config(bg="powderblue")
-        title=Label(self.root,text="Admin Page",bg="powderblue",font=("bold",'20'))
-        title.pack()
-        books_button=Button(self.root,text="Books",command=self.books_page)
-        books_button.place(x=200,y=120)
-        members_button=Button(self.root,text="Members",command=self.member_page)
-        members_button.place(x=400,y=120)
-        transaction_button=Button(self.root,text="Transaction",command=self.transaction_page)
-        transaction_button.place(x=600,y=120)
+        headingFrame1 = Frame(root,bg="#FFBB00",bd=5)
+        headingFrame1.place(relx=0.2,rely=0.1,relwidth=0.6,relheight=0.16)
+        headingLabel = Label(headingFrame1, text="Welcome to \n MEC Library Hyderabad", bg='black', fg='white', font=('Courier',15))
+        headingLabel.place(relx=0,rely=0, relwidth=1, relheight=1)
+        btn1 = Button(self.root,text="Add Book Details",bg='black', fg='white')
+        btn1.place(relx=0.28,rely=0.4, relwidth=0.45,relheight=0.1)
+    
+        btn2 = Button(self.root,text="Delete Book",bg='black', fg='white')
+        btn2.place(relx=0.28,rely=0.5, relwidth=0.45,relheight=0.1)
+    
+        btn3 = Button(self.root,text="View Book List",bg='black', fg='white',command=self.books_page)
+        btn3.place(relx=0.28,rely=0.6, relwidth=0.45,relheight=0.1)
+    
+        btn4 = Button(self.root,text="Issuer Details",bg='black', fg='white',command=self.member_page)
+        btn4.place(relx=0.28,rely=0.7, relwidth=0.45,relheight=0.1)
+    
+        btn5 = Button(self.root,text="Transactions",bg='black', fg='white',command=self.transaction_page)
+        btn5.place(relx=0.28,rely=0.8, relwidth=0.45,relheight=0.1)
+
+       
     def transaction_page(self):
         window=Tk()
         window.title("Transaction Page")
         window.geometry("800x800")
         window.config(bg="powderblue")
-        title=Label(window,text="Transaction Page",bg="powderblue",font=("bold",'20'))
-        title.place(x=300,y=10)
+        headingFrame1 = Frame(window,bg="#FFBB00",bd=5)
+        headingFrame1.place(relx=0.35,rely=0.1,relwidth=0.3,relheight=0.1)
+        headingLabel = Label(headingFrame1, text="Transactions", bg='black', fg='white', font=('Courier',15))
+        headingLabel.place(relx=0,rely=0, relwidth=1, relheight=1)
         book_name_label=Label(window,text="Book Name ",bg="powderblue",font=('bold','12'))
-        book_name_label.place(x=60,y=80)
+        book_name_label.place(x=60,y=200)
 
         author_label=Label(window,text="Issuer ",bg="powderblue",font=('bold','12'))
-        author_label.place(x=300,y=80)
+        author_label.place(x=300,y=200)
     
         qty_label=Label(window,text="Payment Status ",bg="powderblue",font=('bold','12'))
-        qty_label.place(x=460,y=80)
+        qty_label.place(x=460,y=200)
 
         admin_Submit=Button(window,text="Back to HomePage")
-        admin_Submit.place(x=300,y=200)
+        admin_Submit.place(x=300,y=400)
     def member_page(self):
        window=Tk()
        window.title("Members Page")
        window.geometry("800x800")
        window.config(bg="powderblue")
-       title=Label(window,text="Member Page",bg="powderblue",font=("bold",'20'))
-       title.place(x=300,y=10)
+      
+       headingFrame1 = Frame(window,bg="#FFBB00",bd=5)
+       headingFrame1.place(relx=0.35,rely=0.1,relwidth=0.3,relheight=0.1)
+       headingLabel = Label(headingFrame1, text="Members Page", bg='black', fg='white', font=('Courier',15))
+       headingLabel.place(relx=0,rely=0, relwidth=1, relheight=1)
+       
+       labelFrame = Frame(window,bg='black')
+       labelFrame.place(relx=0.1,rely=0.3,relwidth=0.8,relheight=0.5) 
+       lb1 = Label(labelFrame,text="Book ID : ", bg='black', fg='white')
+       lb1.place(relx=0.05,rely=0.2)
+        
+       inf1 = Entry(labelFrame)
+       inf1.place(relx=0.3,rely=0.2, relwidth=0.62)
+    
+    # Issued To Student name 
+       lb2 = Label(labelFrame,text="Issued To : ", bg='black', fg='white')
+       lb2.place(relx=0.05,rely=0.4)
+        
+       inf2 = Entry(labelFrame)
+       inf2.place(relx=0.3,rely=0.4, relwidth=0.62)
+    
+    
+    #Issue Button
+       issueBtn = Button(window,text="Issue",bg='#d1ccc0', fg='black')
+       issueBtn.place(relx=0.28,rely=0.9, relwidth=0.18,relheight=0.08)
+    
+       quitBtn = Button(window,text="Quit",bg='#aaa69d', fg='black')
+       quitBtn.place(relx=0.53,rely=0.9, relwidth=0.18,relheight=0.08)
     def books_page(self):
         window=Tk()
         window.title("Books Page")
         window.geometry("800x800")
         window.config(bg="powderblue")
-        title=Label(window,text="Books Page",bg="powderblue",font=("bold",'20'))
-        title.place(x=300,y=10)
+       
+        headingFrame1 = Frame(window,bg="#FFBB00",bd=5)
+        headingFrame1.place(relx=0.35,rely=0.08,relwidth=0.3,relheight=0.1)
+        headingLabel = Label(headingFrame1, text="View Books List", bg='black', fg='white', font=('Courier',15))
+        headingLabel.place(relx=0,rely=0, relwidth=1, relheight=1)
         search_books=Label(window,text="Search ",bg="powderblue",font=("bold","10"))
-        search_books.place(x=200,y=70)
+        search_books.place(x=270,y=200)
         search_box=Entry(window)
-        search_box.place(x=290,y=70)
+        search_box.place(relwidth=0.25,x=330,y=200)
 root=Tk()
 obj=Multiple(root)
 root.mainloop()
